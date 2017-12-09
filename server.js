995cfd7eeb530f83832f51a479e8f7b05c19f43b
mohan <mohankukreja1@gmail.com>
2017-12-09 18:22:01 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const config=require('config')
+const config=require('./config')
 const path=require('path')
 const app=express()
 app.use('/',express.static(path.join(__dirname,'frontend')))
