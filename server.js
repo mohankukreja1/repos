b48edffac02bfb6a0be8b1e89240e12e6d5aa168
mohan <mohankukreja1@gmail.com>
2017-12-09 18:09:06 +0530

    first comit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const config=require('config')
+const path=require('path')
+const app=express()
+app.use('/',express.static(path.join(__dirname,'frontend')))
+app.get('/hello',(req,res)=>{
+    res.send('hello')
+})
+app.listen(config.port,()=>{
+    console.log('http://localhost:' + config.port)
+
+})
